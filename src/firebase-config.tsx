b1bdab5aa6689f23d7567f549ed46e8519e7da6b
Mateusz <mateusz.grzybowski98@gmail.com>
2022-06-30 11:39:35 +0200

    Created navigation

---
+++ src/firebase-config.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCRQxtXtV1S-D8StPOJNmuLP1n3hwP2Y2k",
+    authDomain: "transaction-tracker-4cee7.firebaseapp.com",
+    projectId: "transaction-tracker-4cee7",
+    storageBucket: "transaction-tracker-4cee7.appspot.com",
+    messagingSenderId: "127227358820",
+    appId: "1:127227358820:web:bb970029ff807887cf3971"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
